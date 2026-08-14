8f6f62b43fa84fcc52346b2e7f546960df0e17ba
Michael King <1747304+tsal@users.noreply.github.com>
2026-08-14 05:43:57 -0400

    chore: release

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+# TODO: discover version from environment / build / tag?
+
 # intel
 rustup target add x86_64-apple-darwin
 # AS
@@ -11,3 +13,16 @@ source .env
 pushd app-rs
 cargo tauri build --bundles app,dmg --target universal-apple-darwin
 
+# staple the app
+xcrun stapler staple "target/universal-apple-darwin/release/bundle/macos/brhap.app"
+
+# notarize dmg
+xcrun notarytool submit "target/universal-apple-darwin/release/bundle/dmg/brhap_0.1.0_universal.dmg" --apple-id "$APPLE_ID" --password "$APPLE_PASSWORD" --team-id "$APPLE_TEAM_ID" --wait
+
+# staple dmg 
+xcrun stapler staple "target/universal-apple-darwin/release/bundle/dmg/brhap_0.1.0_universal.dmg"
+
+# assess (uncomment to debug)
+# spctl --assess --type open --context context:primary-signature -vv "target/universal-apple-darwin/release/bundle/dmg/brhap_0.1.0_universal.dmg"
+
+popd
