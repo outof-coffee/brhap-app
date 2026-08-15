e13e9d186a2847456beb8abe8928dc0379ac4956
Michael King <1747304+tsal@users.noreply.github.com>
2026-08-15 07:28:52 -0400

    chore(release): detect version

--- release.sh
+++ release.sh
@@ -11,16 +11,18 @@ rustup target add aarch64-apple-darwin
 source .env
 
 pushd app-rs
+VERSION=$(cargo metadata --format-version=1 --no-deps | jq '.packages[0].version' --raw-output)
+
 cargo tauri build --bundles app,dmg --target universal-apple-darwin
 
 # staple the app
 xcrun stapler staple "target/universal-apple-darwin/release/bundle/macos/brhap.app"
 
 # notarize dmg
-xcrun notarytool submit "target/universal-apple-darwin/release/bundle/dmg/brhap_0.1.0_universal.dmg" --apple-id "$APPLE_ID" --password "$APPLE_PASSWORD" --team-id "$APPLE_TEAM_ID" --wait
+xcrun notarytool submit "target/universal-apple-darwin/release/bundle/dmg/brhap_${VERSION}_universal.dmg" --apple-id "$APPLE_ID" --password "$APPLE_PASSWORD" --team-id "$APPLE_TEAM_ID" --wait
 
 # staple dmg 
-xcrun stapler staple "target/universal-apple-darwin/release/bundle/dmg/brhap_0.1.0_universal.dmg"
+xcrun stapler staple "target/universal-apple-darwin/release/bundle/dmg/brhap_${VERSION}_universal.dmg"
 
 # assess (uncomment to debug)
 # spctl --assess --type open --context context:primary-signature -vv "target/universal-apple-darwin/release/bundle/dmg/brhap_0.1.0_universal.dmg"
