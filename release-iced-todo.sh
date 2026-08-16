f69efd3405cab22a6aca38344a4c2bf51892a9cc
Michael King <1747304+tsal@users.noreply.github.com>
2026-08-16 11:29:46 -0400

    chore: first pass at iced release for macos

--- release-iced-todo.sh
+++ release-iced-todo.sh
@@ -10,21 +10,42 @@ rustup target add aarch64-apple-darwin
 # local pinning 
 source .env
 
-pushd app-rs
-VERSION=$(cargo metadata --format-version=1 --no-deps | jq '.packages[0].version' --raw-output)
+# unwieldy, maybe we can do this better with cargo metadata and jq
+VERSION=$(cargo metadata --format-version=1 --no-deps | jq '.packages[3].version' --raw-output)
 
-cargo tauri build --bundles app,dmg --target universal-apple-darwin
+cargo build -p brhap-app --target aarch64-apple-darwin
+cargo build -p brhap-app --target x86_64-apple-darwin
 
-# staple the app - this is done by the above when the key is provided correctly
-# xcrun stapler staple "../target/universal-apple-darwin/release/bundle/macos/brhap.app"
+mkdir -p target/universal-apple-darwin/release/bundle/BRhap.app/Contents/MacOS
+mkdir -p target/universal-apple-darwin/release/bundle/BRhap.app/Contents/Resources
 
-# notarize dmg
-xcrun notarytool submit "../target/universal-apple-darwin/release/bundle/dmg/brhap_${VERSION}_universal.dmg" --apple-id "$APPLE_ID" --password "$APPLE_PASSWORD" --team-id "$APPLE_TEAM_ID" --wait
+lipo -create \
+  target/aarch64-apple-darwin/release/brhap-app \
+  target/x86_64-apple-darwin/release/brhap-app \
+  -output target/universal-apple-darwin/release/bundle/BRhap.app/Contents/MacOS/brhap-app
 
-# staple dmg
-xcrun stapler staple "../target/universal-apple-darwin/release/bundle/dmg/brhap_${VERSION}_universal.dmg"
+file target/universal-apple-darwin/release/bundle/BRhap.app/Contents/MacOS/brhap-app # check that it is universal
+
+cp -rp ../app-rs/src-tauri/icons/icons.icns target/universal-apple-darwin/release/bundle/BRhap.app/Contents/Resources/icons.icns
+
+codesign --sign "$APPLE_IDENTITY" --timestamp --options runtime --deep --force target/universal-apple-darwin/release/bundle/BRhap.app/Contents/MacOS/brhap-app
+
+# TODO: brew install create-dmg
+create-dmg \
+    --volname "BRhap ${VERSION}" \
+    --volicon "target/universal-apple-darwin/release/bundle/BRhap.app/Contents/Resources/icons.icns" \
+    --window-pos 200 120 \
+    --window-size 600 400 \
+    --icon-size 100 \
+    --icon "BRhap.app" 175 120 \
+    --app-drop-link 425 120 \
+    "target/universal-apple-darwin/release/bundle/dmg/brhap_${VERSION}_universal.dmg" \
+    "target/universal-apple-darwin/release/bundle/BRhap.app"
+
+codesign --sign "$APPLE_IDENTITY" --timestamp --options runtime --deep --force target/universal-apple-darwin/release/bundle/dmg/brhap_${VERSION}_universal.dmg
+
+xcrun notarytool submit "target/universal-apple-darwin/release/bundle/dmg/brhap_${VERSION}_universal.dmg" --apple-id "$APPLE_ID" --password "$APPLE_PASSWORD" --team-id "$APPLE_TEAM_ID" --wait
+xcrun stapler staple "target/universal-apple-darwin/release/bundle/dmg/brhap_${VERSION}_universal.dmg"
 
 # assess (uncomment to debug)
-# spctl --assess --type open --context context:primary-signature -vv "../target/universal-apple-darwin/release/bundle/dmg/brhap_0.1.0_universal.dmg"
-
-popd
+spctl --assess --type open --context context:primary-signature -vv "target/universal-apple-darwin/release/bundle/dmg/brhap_${VERSION}_universal.dmg"
